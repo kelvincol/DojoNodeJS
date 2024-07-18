28fd9ce56c107978f6127aab58de31b6176a8217
kelvincol <plus-93@hotmail.com>
2024-07-18 19:47:31 -0300

    atualizando versoes

--- src/fileUtils/fileUtils.js
+++ src/fileUtils/fileUtils.js
@@ -1,12 +1,9 @@
-import fs from 'fs'
+
 import path from 'path'
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
+import { __filename } from './fileSystem.js'
 
 
-const readFile = (fileName) => {
-    return fs.readFileSync(fileName, 'utf8')
-}
+
 
 const getOutputPath = () => { 
     const __dirname = path.dirname(__filename) 
@@ -24,36 +21,6 @@ const generateDefaultOutputFilePath = (fileName, fileNumber, fileExtension) => {
 
 
 
-const totalFilesInFolder = (folderPath = getOutputPath()) => {
-    const result = fs.readdirSync(folderPath)
-
-    return result ? result.length : 1
-}
-
-const readFilesInFolder = (folderPath = getOutputPath()) => {
-    return fs.readdirSync(folderPath)
-}
-
-const saveFile = async (fileContent, finalPath) => {
-    const onlyPath = path.dirname(finalPath)
-    
-    if (!fs.existsSync(onlyPath)){
-        await fs.promises.mkdir(onlyPath, { recursive: true })
-    } 
-   
-    fs.writeFileSync(finalPath, fileContent)
-}
-
-const saveFileText = (inputFileName, fileNumber, fileContent) => {
-    const fileNameWithPath = generateDefaultOutputFilePath(inputFileName, fileNumber, 'txt')
-
-    fs.writeFileSync(fileNameWithPath, fileContent)
-}
-
-const saveJSONFile = async (fileContent, finalPath) => {
-    await saveFile(JSON.stringify(fileContent, null, 4), finalPath)
-}
-
-
-
-export { readFile, saveFileText, totalFilesInFolder, generateDefaultOutputFilePath, readFilesInFolder, saveFile, saveJSONFile }
+export {
+         generateDefaultOutputFilePath
+         }
