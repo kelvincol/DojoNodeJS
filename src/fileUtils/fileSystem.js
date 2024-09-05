ab253d043ef9f7109597dbc5b1e6970e33859f7c
kelvincol <plus-93@hotmail.com>
2024-09-05 19:51:36 -0300

    atualizando backup

--- src/fileUtils/fileSystem.js
+++
@@ -1,5 +0,0 @@
-import { fileURLToPath } from 'url';
-//TODO: ajustar erro contexto de testes
-//const __filename = fileURLToPath(import.meta.url);
-const __filename = 'C:\\node ze lessons\\DojoNodeJS\\src'
-export {__filename}
